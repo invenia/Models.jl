40c60a0b324c27472b08d6ddfbb81084cd71d2a8
Will Tebbutt <will.tebbutt@invenialabs.co.uk>
2020-08-07 10:28:17 +0100

    Remove references to posterior

--- src/traits.jl
+++ src/traits.jl
@@ -17,7 +17,7 @@ abstract type PointEstimate <: EstimateTrait end
 """
     DistributionEstimate <: EstimateTrait
 
-Specifies that the [`Model`](@ref) returns a posterior distribution over the response variables.
+Specifies that the [`Model`](@ref) returns a distribution over the response variables.
 """
 abstract type DistributionEstimate <: EstimateTrait end
 
