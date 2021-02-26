f63f9f5c689afb3a675bdf7d2097d7e87279da2e
Branwen Snelling <branwen.snelling@invenialabs.co.uk>
2021-02-26 11:47:24 +0000

    test test utils in iterative set

--- test/test_utils.jl
+++ test/test_utils.jl
@@ -1,65 +1,17 @@
 @testset "test_utils.jl" begin
 
-    @testset "FakeTemplate{PointEstimate, SingleOutput, PointPredictInput}" begin
-        temp = FakeTemplate{PointEstimate, SingleOutput, PointPredictInput}()
-        test_interface(temp)
+    estimates = (PointEstimate, DistributionEstimate)
+    outputs = (SingleOutput, MultiOutput)
+    predict_inputs = (PointPredictInput, DistributionPredictInput, PointOrDistributionPredictInput)
 
+    @testset "$est, $out, $pin" for (est, out, pin) in Iterators.product(estimates, outputs, predict_inputs)
+        temp = FakeTemplate{est, out, pin}()
+        test_interface(temp)
+    end    
+
+    @testset "Vector inputs case" begin
         temp = FakeTemplate{PointEstimate, SingleOutput, PointPredictInput}()
         test_interface(temp; inputs=[rand(5), rand(5)], outputs=rand(1, 2))
     end
-
-    @testset "FakeTemplate{PointEstimate, MultiOutput, PointPredictInput}" begin
-        temp = FakeTemplate{PointEstimate, MultiOutput, PointPredictInput}()
-        test_interface(temp)
-    end
-
-    @testset "FakeTemplate{DistributionEstimate, SingleOutput, PointPredictInput}" begin
-        temp = FakeTemplate{DistributionEstimate, SingleOutput, PointPredictInput}()
-        test_interface(temp)
-    end
-
-    @testset "FakeTemplate{DistributionEstimate, MultiOutput, PointPredictInput}" begin
-        temp = FakeTemplate{DistributionEstimate, MultiOutput, PointPredictInput}()
-        test_interface(temp)
-    end
-
-    @testset "FakeTemplate{PointEstimate, SingleOutput, DistributionPredictInput}" begin
-        temp = FakeTemplate{PointEstimate, SingleOutput, DistributionPredictInput}()
-        test_interface(temp)
-    end
-
-    @testset "FakeTemplate{PointEstimate, MultiOutput, DistributionPredictInput}" begin
-        temp = FakeTemplate{PointEstimate, MultiOutput, DistributionPredictInput}()
-        test_interface(temp)
-    end
-
-    @testset "FakeTemplate{DistributionEstimate, SingleOutput, DistributionPredictInput}" begin
-        temp = FakeTemplate{DistributionEstimate, SingleOutput, DistributionPredictInput}()
-        test_interface(temp)
-    end
-
-    @testset "FakeTemplate{DistributionEstimate, MultiOutput, DistributionPredictInput}" begin
-        temp = FakeTemplate{DistributionEstimate, MultiOutput, DistributionPredictInput}()
-        test_interface(temp)
-    end
-
-    @testset "FakeTemplate{PointEstimate, SingleOutput, PointOrDistributionPredictInput}" begin
-        temp = FakeTemplate{PointEstimate, SingleOutput, PointOrDistributionPredictInput}()
-        test_interface(temp)
-    end
-
-    @testset "FakeTemplate{PointEstimate, MultiOutput, PointOrDistributionPredictInput}" begin
-        temp = FakeTemplate{PointEstimate, MultiOutput, PointOrDistributionPredictInput}()
-        test_interface(temp)
-    end
-
-    @testset "FakeTemplate{DistributionEstimate, SingleOutput, PointOrDistributionPredictInput}" begin
-        temp = FakeTemplate{DistributionEstimate, SingleOutput, PointOrDistributionPredictInput}()
-        test_interface(temp)
-    end
-
-    @testset "FakeTemplate{DistributionEstimate, MultiOutput, PointOrDistributionPredictInput}" begin
-        temp = FakeTemplate{DistributionEstimate, MultiOutput, PointOrDistributionPredictInput}()
-        test_interface(temp)
-    end
+    
 end
