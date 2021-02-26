9007b2b8556271ac7ff4d04cc6b0871f28fc1038
Branwen Snelling <branwen.snelling@invenialabs.co.uk>
2021-02-26 13:58:53 +0000

    Document predict functions

--- src/Models.jl
+++ src/Models.jl
@@ -46,6 +46,10 @@ function fit end
 
 Predict targets for the provided the collection of `inputs` and [`Model`](@ref).
 
+A [`Model`](@ref) subtype for which the `predict_input_type(model)` is 
+[`PointPredictInput`](@ref) will only need to implement a `predict` function that operates 
+on an `AbstractMatrix` of inputs.  
+
 If the `estimate_type(model)` is [`PointEstimate`](@ref) then this function should return
 another `AbstractMatrix` in which each column contains the prediction for a single input.
 
